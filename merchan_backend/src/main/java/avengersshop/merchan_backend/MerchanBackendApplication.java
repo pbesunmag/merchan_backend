423010924a62aa7d3e72be8bc92d430dd1990e0e
pol.bastida <polbastidagonzalez@gmail.com>
2026-08-06 21:22:45 +0200

    Base del proyecto creada

---
+++ merchan_backend/src/main/java/avengersshop/merchan_backend/MerchanBackendApplication.java
@@ -0,0 +1,13 @@
+package avengersshop.merchan_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerchanBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerchanBackendApplication.class, args);
+	}
+
+}
