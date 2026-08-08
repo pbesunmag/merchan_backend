26c69c458ea2800c350aedefaef9358876b1f09e
linnethrodrigues <linnethrp@gmail.com>
2026-08-08 15:05:45 +0200

    repositorio, controlador y servicio de Categorías

--- merchan_backend/src/main/java/avengersshop/merchan_backend/services/AvengersShopService.java
+++ merchan_backend/src/main/java/avengersshop/merchan_backend/services/AvengersShopService.java
@@ -1,8 +1,12 @@
 package avengersshop.merchan_backend.services;
 
+import avengersshop.merchan_backend.dto.request.CrearCategoriaDTO;
 import avengersshop.merchan_backend.dto.request.CrearTerminalDTO;
+import avengersshop.merchan_backend.dto.response.CategoriaDTO;
 import avengersshop.merchan_backend.dto.response.TerminalDTO;
+import avengersshop.merchan_backend.models.Categoria;
 import avengersshop.merchan_backend.models.Terminal;
+import avengersshop.merchan_backend.repositories.ICategoriaRepository;
 import avengersshop.merchan_backend.repositories.ITerminalRepository;
 import org.springframework.stereotype.Service;
 
@@ -12,11 +16,14 @@ import java.util.List;
 public class AvengersShopService {
 
     private final ITerminalRepository iTerminalRepository;
+    private final ICategoriaRepository iCategoriaRepository;
 
-    public AvengersShopService(ITerminalRepository iTerminalRepository) {
+    public AvengersShopService(ITerminalRepository iTerminalRepository, ICategoriaRepository iCategoriaRepository) {
         this.iTerminalRepository = iTerminalRepository;
+        this.iCategoriaRepository = iCategoriaRepository;
     }
 
+    //MÉTODOS DE TERMINALES
     //Listamos todas las terminales
     public List<TerminalDTO> listarTerminales() {
         return iTerminalRepository.findAll().stream()
@@ -31,6 +38,23 @@ public class AvengersShopService {
         Terminal terminalGuardada = iTerminalRepository.save(terminal);
         //Convertimos el modelo guardado a TerminalDTO para devolverla
         return TerminalDTO.fromEntity(terminalGuardada);
+    }
+
+    //MÉTODOS DE CATEGORÍAS
+    //Listamos todas las categorías
+    public List<CategoriaDTO> listarCategorias() {
+        return iCategoriaRepository.findAll().stream()
+                .map(CategoriaDTO::fromEntity).toList();
+    }
+
+    //Creamos nueva categoría
+    public CategoriaDTO crearCategoria(CrearCategoriaDTO categoriaDto) {
+        Categoria categoria = new Categoria();
+        categoria.setNombre(categoriaDto.getNombre());
+        categoria.setDescripcion(categoriaDto.getDescripcion());
+
+        Categoria categoriaGuardada = iCategoriaRepository.save(categoria);
+        return CategoriaDTO.fromEntity(categoriaGuardada);
     }
 
 
